efe280cb44da13ab5ece1feef9d1a2cb1df066c4
celetx <celestineosisiogu@gmail.com>
2020-11-26 23:41:39 +0100

    added if condition to query a data based on catrgory or item_name

--- stockmgmt/views.py
+++ stockmgmt/views.py
@@ -9,17 +9,28 @@ def home(request):
 	context = {
 	    "title": title,
 	}
-	return render(request, "stockmgmt/home.html",context)
+	return render(request, "stockmgmt/home.html",context) 
+
+
 
 
 def list_item(request):
-	header = 'List of Items'
-	queryset = Stock.objects.all()
-	context = {
-		"header": header,
-		"queryset": queryset,
-	}
-	return render(request, "stockmgmt/list_item.html", context)
+        header = 'List of Items'
+        queryset = Stock.objects.all()
+        form = StockSearchForm(request.POST or None)
+        context = {
+            "header": header,
+            "queryset": queryset,
+        }
+        if request.method == 'POST':
+            queryset = Stock.objects.filter(category__icontains=form['category'].value(),
+            item_name__icontains=form['item_name'].value())
+        context = {
+            "form": form,
+            "header": header,
+            "queryset": queryset,
+        }    
+        return render(request, "stockmgmt/list_item.html", context)
 
 
 def add_items(request):
