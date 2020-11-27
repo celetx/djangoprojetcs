55aa21452a55f4cdeb860ca7a4b13cdc83d2fb62
celetx <celestineosisiogu@gmail.com>
2020-11-27 04:47:32 +0100

    validated category field on forms.py

--- stockmgmt/forms.py
+++ stockmgmt/forms.py
@@ -6,6 +6,13 @@ class StockCreateForm(forms.ModelForm):
    class Meta:
      model = Stock
      fields = ['category', 'item_name', 'quantity']
+
+     def clean_category(self):
+       category = self.cleaned_data.get('category')
+       if not category:
+         raise forms.ValidationError('This Field is Required')
+         return category
+
      
 
 class StockSearchForm(forms.ModelForm):
