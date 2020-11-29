36e34bad30938372fa7a235c2c20d216833df6b9
celetx <celestineosisiogu@gmail.com>
2020-11-29 23:10:22 +0100

    added delete_items url path

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('list_item/', views.list_item, name='list_item'),
     path('add_items/', views.add_items, name='add_items'),
-    path('update_items/<str:pk>', views.update_items, name='update_items')
+    path('update_items/<str:pk>', views.update_items, name='update_items'),
+    path('delete_items/<str:pk>/', views.delete_items, name="delete_items"),
 ]
 
 
